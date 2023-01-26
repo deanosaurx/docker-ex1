ab777d1e68d0b1f5c10479547c9f0d97022b19c1
Dean Sorie <deansorie@icloud.com>
2023-01-26 10:57:31 +0200

    Created a connection to mongo atlas, now defining a mongo schema

--- nodeApp/config/db.js
+++ nodeApp/config/db.js
@@ -1,1 +1,10 @@
 const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+const connectDB = async () => {
+  const db = await mongoose.connect(process.env.MONGO_DB_URI, {});
+  console.log("connected!");
+};
+
+module.exports = connectDB;
